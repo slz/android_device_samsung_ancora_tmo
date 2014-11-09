7b32e3cfb0dd16e067c17d2126cedb85f44f4f8b
doadin <tbkizle@gmail.com>
2014-11-09 06:32:42 -0500

    Fix Typos

--- ancora_tmo.mk
+++ ancora_tmo.mk
@@ -45,7 +45,7 @@ PRODUCT_COPY_FILES += \
 # Media configuration
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_codecs.xml:system/etc/media_codecs.xml \
-    $(LOCAL_PATH)/media/media_profiles.xml:system/etc/media_profiles.xml \
+    $(LOCAL_PATH)/media/media_profiles.xml:system/etc/media_profiles.xml
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
