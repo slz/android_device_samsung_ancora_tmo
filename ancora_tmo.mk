a02550bee9670d18e9ed151ac10b4d6bac609f28
doadin <tbkizle@gmail.com>
2014-12-21 20:00:20 -0500

    Fix Typo

--- ancora_tmo.mk
+++ ancora_tmo.mk
@@ -48,7 +48,7 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml \
-    $(COMMON_PATH)/rootdir/system/etc/media_codecs.xml:system/etc/media_codecs.xml
+    $(LOCAL_PATH)/media/media_codecs.xml:system/etc/media_codecs.xml
 
 # Media configuration
 PRODUCT_COPY_FILES += \
