bd74d9920c8a5c76493d2da1e4c7c7004c52757e
slz <madskiilz@yahoo.com>
2015-08-01 12:04:47 -0400

    audio: use 44.1 kHz UI sounds
    Our fast paths sample rate is 44.1 kHz, which
    conflicts with the default 48 kHz sounds introduced in KK.

--- full_ancora_tmo.mk
+++ full_ancora_tmo.mk
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# 44.1 kHz sounds
+$(call inherit-product-if-exists, frameworks/base/data/sounds/AudioPackage12.mk)
+
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(LOCAL_PATH)/ancora_tmo.mk)
 
